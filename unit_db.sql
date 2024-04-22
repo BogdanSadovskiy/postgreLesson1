e3b4e04ce6437e874d3b861c70c9363eb617bf66
BogdanSadovskiy <36935732+BogdanSadovskiy@users.noreply.github.com>
2024-04-22 17:44:47 +0300

    Update unit_db.sql

--- unit_db.sql
+++ unit_db.sql
@@ -1,74 +1,71 @@
-create table if not exists departments(
-id serial primary key,
-name varchar(50),
-building int
-);
-create table if not exists wards(
-id serial primary key,
-name varchar(50),
-places int,
-department_id int, 
- foreign key (department_id) references departments(id)
+CREATE TABLE IF NOT EXISTS departments (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    building INT
 );
 
-create table if not exists doctors(
-id serial primary key,
-name varchar(50),
-surname varchar(50),
-premium int,
-salary int
+CREATE TABLE IF NOT EXISTS wards (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    places INT,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-create table if not exists examinations(
-id serial primary key,
-name varchar(50)
+CREATE TABLE IF NOT EXISTS doctors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    surname VARCHAR(50),
+    premium INT,
+    salary INT
 );
 
-create table if not exists doctors_examinations(
-id serial primary key,
-end_time time,
-start_time time,
-doctor_id int,
-examination_id int,
-ward_id int,
-foreign key (examination_id) references examinations(id),
-foreign key (doctor_id) references doctors(id),
-foreign key (ward_id) references wards(id)
+CREATE TABLE IF NOT EXISTS examinations (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS doctors_examinations (
+    id SERIAL PRIMARY KEY,
+    end_time TIME,
+    start_time TIME,
+    doctor_id INT,
+    examination_id INT,
+    ward_id INT,
+    FOREIGN KEY (examination_id) REFERENCES examinations(id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id),
+    FOREIGN KEY (ward_id) REFERENCES wards(id)
 );
 
 INSERT INTO departments (name, building) VALUES
-    ('Педіатрія', 1),
-    ('Травматологія', 2),
-    ('Неврологія', 3);
-
+    ('Pediatrics', 1),
+    ('Traumatology', 2),
+    ('Neurology', 3);
 
 INSERT INTO doctors (name, surname, premium, salary) VALUES
-    ('Іван', 'Петров', 0, 3000),
-    ('Марія', 'Іванова', 600, 3500),
-    ('Олег', 'Сидоров', 700, 4000);
-
+    ('John', 'Smith', 0, 3000),
+    ('Mary', 'Johnson', 600, 3500),
+    ('Oliver', 'Davis', 700, 4000);
 
 INSERT INTO examinations (name) VALUES
-    ('Рентген'),
-    ('УЗД'),
-    ('Аналіз крові');
-
+    ('X-ray'),
+    ('Ultrasound'),
+    ('Blood Test');
 
 INSERT INTO wards (name, places, department_id) VALUES
-    ('Палата Педіатрії №1', 10, 1),
-    ('Палата Педіатрії №2', 8, 1),
-    ('Палата Педіатрії №3', 8, 1),
-    ('Палата Педіатрії №4', 2, 1),
-    ('Палата Травматології №1', 8, 2),
-    ('Палата Травматології №2', 15, 2),
-    ('Палата Травматології №3', 8, 2),
-    ('Палата Травматології №4', 2, 2),
-    ('Палата Неврології №1', 5, 3),
-    ('Палата Неврології №2', 5, 3),
-    ('Палата Неврології №3', 5, 3),
-    ('Палата Неврології №4', 5, 3),
-    ('Палата Неврології №5', 1, 3);
-
+    ('Pediatrics Ward #1', 10, 1),
+    ('Pediatrics Ward #2', 8, 1),
+    ('Pediatrics Ward #3', 8, 1),
+    ('Pediatrics Ward #4', 2, 1),
+    ('Traumatology Ward #1', 8, 2),
+    ('Traumatology Ward #2', 15, 2),
+    ('Traumatology Ward #3', 8, 2),
+    ('Traumatology Ward #4', 2, 2),
+    ('Neurology Ward #1', 5, 3),
+    ('Neurology Ward #2', 5, 3),
+    ('Neurology Ward #3', 5, 3),
+    ('Neurology Ward #4', 5, 3),
+    ('Neurology Ward #5', 1, 3);
 
 INSERT INTO doctors_examinations (end_time, start_time, doctor_id, examination_id, ward_id) VALUES
     ('10:00', '09:00', 1, 1, 1),
